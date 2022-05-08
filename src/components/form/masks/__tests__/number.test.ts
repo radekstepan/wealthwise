773931498561aed48fd1a605ed1c08ab6aa47951
Radek Stepan <dev@radekstepan.com>
2022-05-07 22:33:49 -0700

    feat: range number mask

--- src/components/form/masks/__tests__/number.test.ts
+++ src/components/form/masks/__tests__/number.test.ts
@@ -1,16 +1,33 @@
 import {currencyMask} from '../number';
 
 describe('currencyMask', () => {
-  test('currency', () => {
+  test('', () => {
+    expect(currencyMask('')).toEqual([
+      '$', /\d/
+    ]);
+  });
+
+  test('$3,000.59', () => {
     expect(currencyMask('$3,000.59')).toEqual([
       '$', /\d/, ',', /\d/, /\d/, /\d/, '[]', '.', '[]', /\d/, /\d/
     ]);
   });
 
-  // TODO
-  test('currency range', () => {
-    expect(currencyMask('$3,000.59')).toEqual([
-      '$', /\d/, ',', /\d/, /\d/, /\d/, '[]', '.', '[]', /\d/, /\d/
+  test('$3␣-␣$5', () => {
+    expect(currencyMask('$3 - $5')).toEqual([
+      '$', /\d/, ' ', '-', ' ', '$', /\d/
+    ]);
+  });
+
+  test('$3␣-', () => {
+    expect(currencyMask('$3 -')).toEqual([
+      '$', /\d/, ' ', '-', ' ', '$', /\d/
+    ]);
+  });
+
+  test('$3␣', () => {
+    expect(currencyMask('$3 ')).toEqual([
+      '$', /\d/, ' '
     ]);
   });
 });
--- src/components/form/masks/number.ts
+++ src/components/form/masks/number.ts
@@ -1,153 +1,158 @@
-const dollarSign = '$';
-const emptyString = '';
-const comma = ',';
-const period = '.';
-const minus = '-';
-const minusRegExp = /-/;
-const nonDigitsRegExp = /\D+/g;
-const number = 'number';
-const digitRegExp = /\d/;
-const caretTrap = '[]';
+const EMPTY = '';
+const REGEX_NON_DIGITS = /\D+/g;
+const REGEX_DIGIT = /\d/;
+const CARET_TRAP = '[]';
+const RANGE_TRIGGER = ' -';
+const RANGE_SEPARATOR = ' - ';
+
+type Mask = (string | RegExp)[];
+
+// An array of chars and regex digits.
+const convertToMask = (text: string): Mask => text
+  .split(EMPTY)
+  .map(char => REGEX_DIGIT.test(char) ? REGEX_DIGIT : char);
+
+// http://stackoverflow.com/a/10899795/604296
+const addThousandsSeparator = (text: string, separator: string) => text
+  .replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 
 export default function createNumberMask({
-  prefix = dollarSign,
-  suffix = emptyString,
+  // currency
+  prefix = '$',
+  suffix = EMPTY,
   includeThousandsSeparator = true,
-  thousandsSeparatorSymbol = comma,
+  thousandsSeparatorSymbol = ',',
   allowDecimal = false,
-  decimalSymbol = period,
+  decimalSymbol = '.',
   decimalLimit = 2,
+
   requireDecimal = false,
   allowNegative = false,
   allowLeadingZeroes = false,
-  integerLimit = null
+  integerLimit = null, // TODO needs to work for a range
+  allowRange = false // 2 numbers separated by a ' - '
 } = {}) {
   const prefixLength = prefix && prefix.length || 0;
   const suffixLength = suffix && suffix.length || 0;
   const thousandsSeparatorSymbolLength = thousandsSeparatorSymbol && thousandsSeparatorSymbol.length || 0;
 
-  function numberMask(rawValue = emptyString) {
-    const rawValueLength = rawValue.length;
+  return function parse(text = EMPTY) {
+    const length = text.length;
 
-    if (
-      rawValue === emptyString ||
-      (rawValue[0] === prefix[0] && rawValueLength === 1)
-    ) {
-      return [...prefix.split(emptyString), digitRegExp, ...suffix.split(emptyString)];
-    } else if(
-      rawValue === decimalSymbol &&
-      allowDecimal
-    ) {
-      return [...prefix.split(emptyString), '0', decimalSymbol, digitRegExp, ...suffix.split(emptyString)];
+    // Empty or just the prefix.
+    if (text === EMPTY || (text[0] === prefix[0] && length === 1)) {
+      return [...prefix.split(EMPTY), REGEX_DIGIT, ...suffix.split(EMPTY)];
+    // Insert a zero in front of a decimal separator.
+    } else if (allowDecimal && text === decimalSymbol) {
+      return [...prefix.split(EMPTY), '0', decimalSymbol, REGEX_DIGIT, ...suffix.split(EMPTY)];
     }
 
-    const isNegative = (rawValue[0] === minus) && allowNegative;
-    //If negative remove "-" sign
-    if (isNegative) {
-      rawValue = rawValue.toString().substring(1);
-    }
-
-    const indexOfLastDecimal = rawValue.lastIndexOf(decimalSymbol);
-    const hasDecimal = indexOfLastDecimal !== -1;
-
-    let integer;
-    let fraction;
-    let mask;
-
-    // remove the suffix
-    if (rawValue.slice(suffixLength * -1) === suffix) {
-      rawValue = rawValue.slice(0, suffixLength * -1);
-    }
-
-    if (hasDecimal && (allowDecimal || requireDecimal)) {
-      integer = rawValue.slice(rawValue.slice(0, prefixLength) === prefix ? prefixLength : 0, indexOfLastDecimal);
-
-      fraction = rawValue.slice(indexOfLastDecimal + 1, rawValueLength);
-      fraction = convertToMask(fraction.replace(nonDigitsRegExp, emptyString));
-    } else {
-      if (rawValue.slice(0, prefixLength) === prefix) {
-        integer = rawValue.slice(prefixLength);
-      } else {
-        integer = rawValue;
+    if (allowRange) {
+      if (text.includes(RANGE_TRIGGER)) {
+        // TODO handle (disallow) multiple ranges.
+        const [a, b] = text.split(RANGE_TRIGGER).map(d => d.trim()).map(parse);
+        return [...a, ...RANGE_SEPARATOR.split(EMPTY), ...b];
+      }
+      // Allow a trailing space.
+      if (length > 1 && text[length - 1] === ' ') {
+        return [...parse(text.substring(0, length - 1)), ' '];
       }
     }
 
-    if (integerLimit && typeof integerLimit === number) {
+
+    const isNegative = allowNegative && (text[0] === '-');
+    // If negative remove "-" sign.
+    if (isNegative) {
+      text = text.substring(1);
+    }
+
+    const indexOfLastDecimal = text.lastIndexOf(decimalSymbol);
+    const hasDecimal = indexOfLastDecimal >= 0;
+
+    // Remove the suffix.
+    if (text.slice(suffixLength * -1) === suffix) {
+      text = text.slice(0, suffixLength * -1);
+    }
+
+    let integer: string;
+    let fraction: Mask;
+    // Grab the integer value (and any decimals).
+    if (hasDecimal && (allowDecimal || requireDecimal)) {
+      integer = text.slice(text.slice(0, prefixLength) === prefix ? prefixLength : 0, indexOfLastDecimal);
+      fraction = convertToMask(text
+        .slice(indexOfLastDecimal + 1, length)
+        .replace(REGEX_NON_DIGITS, EMPTY)
+      );
+    } else {
+      if (text.slice(0, prefixLength) === prefix) {
+        integer = text.slice(prefixLength);
+      } else {
+        integer = text;
+      }
+    }
+
+    // Trim?
+    if (typeof integerLimit === 'number') {
       const thousandsSeparatorRegex = thousandsSeparatorSymbol === '.' ? '[.]' : `${thousandsSeparatorSymbol}`;
       const numberOfThousandSeparators = (integer.match(new RegExp(thousandsSeparatorRegex, 'g')) || []).length;
 
       integer = integer.slice(0, integerLimit + (numberOfThousandSeparators * thousandsSeparatorSymbolLength));
     }
 
-    integer = integer.replace(nonDigitsRegExp, emptyString);
-
+    // Remove any non digit chars.
+    integer = integer.replace(REGEX_NON_DIGITS, EMPTY);
+    // Remove leading zeroes?
     if (!allowLeadingZeroes) {
       integer = integer.replace(/^0+(0$|[^0])/, '$1');
     }
-
+    // Add the thousands separator.
     integer = (includeThousandsSeparator) ? addThousandsSeparator(integer, thousandsSeparatorSymbol) : integer;
 
-    mask = convertToMask(integer);
+    let mask: Mask = convertToMask(integer);
 
     if ((hasDecimal && allowDecimal) || requireDecimal === true) {
-      if (rawValue[indexOfLastDecimal - 1] !== decimalSymbol) {
-        mask.push(caretTrap);
+      if (text[indexOfLastDecimal - 1] !== decimalSymbol) {
+        mask.push(CARET_TRAP);
       }
 
-      mask.push(decimalSymbol, caretTrap);
+      mask.push(decimalSymbol, CARET_TRAP);
 
+      // Trim and add the fraction.
       if (fraction) {
-        if (typeof decimalLimit === number) {
-          fraction = fraction.slice(0, decimalLimit);
-        }
-
-        mask = mask.concat(fraction);
+        fraction = fraction.slice(0, decimalLimit);
+        mask = [...mask, ...fraction];
       }
 
-      if (requireDecimal === true && rawValue[indexOfLastDecimal - 1] === decimalSymbol) {
-        mask.push(digitRegExp);
+      if (requireDecimal === true && text[indexOfLastDecimal - 1] === decimalSymbol) {
+        mask.push(REGEX_DIGIT);
       }
     }
 
-    if (prefixLength > 0) {
-      mask = prefix.split(emptyString).concat(mask);
+    // Add the prefix to the mask?
+    if (prefixLength) {
+      mask = [...prefix.split(EMPTY), ...mask];
     }
 
     if (isNegative) {
       // If user is entering a negative number, add a mask placeholder spot to attract the caret to it.
       if (mask.length === prefixLength) {
-        mask.push(digitRegExp);
+        mask.push(REGEX_DIGIT);
       }
 
-      mask = [minusRegExp].concat(mask);
+      mask = [/-/, ...mask];
     }
 
-    if (suffix.length > 0) {
-      mask = mask.concat(suffix.split(emptyString));
+    // Add the suffix to the mask?
+    if (suffix.length) {
+      mask = [...mask, ...suffix.split(EMPTY)];
     }
 
     return mask;
-  }
-
-  numberMask.instanceOf = 'createNumberMask';
-
-  return numberMask;
-}
-
-function convertToMask(strNumber) {
-  return strNumber
-    .split(emptyString)
-    .map((char) => digitRegExp.test(char) ? digitRegExp : char);
-}
-
-// http://stackoverflow.com/a/10899795/604296
-function addThousandsSeparator(n, thousandsSeparatorSymbol) {
-  return n.replace(/\B(?=(\d{3})+(?!\d))/g, thousandsSeparatorSymbol);
+  };
 }
 
 export const currencyMask = createNumberMask({
   prefix: '$',
-  suffix: '',
   includeThousandsSeparator: true,
   thousandsSeparatorSymbol: ',',
   allowDecimal: true,
@@ -155,5 +160,6 @@ export const currencyMask = createNumberMask({
   decimalLimit: 2, // how many digits allowed after the decimal
   integerLimit: 7, // limit length of integer numbers
   allowNegative: false,
-  allowLeadingZeroes: false
+  allowLeadingZeroes: false,
+  allowRange: true
 });
