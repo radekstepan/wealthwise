ad307293d8624378b2660a3e50517082e4f9b2f6
Radek Stepan <dev@radekstepan.com>
2022-06-01 21:05:27 -0700

    feat: distribution %

--- src/components/dist/Dist.jsx
+++ src/components/dist/Dist.jsx
@@ -50,7 +50,9 @@ const init = (ref) => {
 const update = (svg, x, xAxis, y, yAxis, data) => {
   const {width, height} = svg.node().getBoundingClientRect();
 
-  const max = data.reduce((m, d) => d[1] > m ? d[1] : m, -Infinity);
+  const [max, sum] = data.reduce(
+    ([m, s], d) => d[1] > m ? [d[1], s + d[1]] : [m, s + d[1]]
+  , [-Infinity, 0]);
 
   x.domain(data.map(d => d[0]));
   y.domain([0, max]);
@@ -65,9 +67,10 @@ const update = (svg, x, xAxis, y, yAxis, data) => {
   //   .duration(500)
   //   .call(yAxis);
 
-  const dist = svg.selectAll('.bar')
-    .data(data)
-    
+  const dist = svg.selectAll('.bar').data(data);
+  
+  const barWidth = (width / data.length) - 30;
+
   dist
     .enter()
     .append("rect")
@@ -79,8 +82,21 @@ const update = (svg, x, xAxis, y, yAxis, data) => {
     .duration(500)
     .attr("x", d => x(d[0]) + 15)
     .attr("y", d => y(d[1]))
-    .attr("width", (width / data.length) - 30)
+    .attr("width", barWidth)
     .attr("height", d => height - y(d[1]));
+  
+  const text = svg.selectAll('.text').data(data);
+
+  text
+    .enter()
+    .append("text")
+    .attr('class', 'text')
+    .merge(text)
+    .transition()
+    .duration(500)
+    .attr("x", d => x(d[0]) + 15 + (barWidth / 2))
+    .attr("y", d => y(d[1]) - 5)
+    .text(d => (d[1] / sum * 100).toFixed(2) + '%');
 }
 
 function Distribution({dist}) {
